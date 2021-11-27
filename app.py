73bc7d0bdbfbb1e3c647019a06448940176abccb
Viktor Kolesnikov <kalach72ru@yandex.ru>
2021-11-27 20:08:36 +0500

    установлен venv и flask

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask
+import requests
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+autodesk_auth_url = 'https://developer.api.autodesk.com/authentication/v1/authenticate'
+
+
+def autodesk_auth_l2():
+    url = autodesk_auth_url
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    params = {
+        'client_id': 'ltdObTmAiCIQNwCuLeQGFXzV7lJLdIy9',
+        'client_secret': 'CZQjy8932uRGvKKS',
+        'grant_type': 'client_credentials',
+        'scope': 'data:read'
+    }
+    req = requests.post(url, data=params, headers=headers)
+    print(req.text)
+
+
+def main():
+    print(f'Программа запущена.')
+    autodesk_auth_l2()
+
+
+if __name__ == '__main__':
+    main()
