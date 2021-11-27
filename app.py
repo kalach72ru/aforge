e102f33de2e1a4c25e4abef9ed2eb59ed718955e
Viktor Kolesnikov <kalach72ru@yandex.ru>
2021-11-27 21:19:13 +0500

    добавлен шаблон главной страницы и форма загрузки файла

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
+
+
+autodesk_auth_url = 'https://developer.api.autodesk.com/authentication/v1/authenticate'
 
 
 app = Flask(__name__)
@@ -7,10 +10,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-
-autodesk_auth_url = 'https://developer.api.autodesk.com/authentication/v1/authenticate'
+    # text = autodesk_auth_l2()
+    text = '{"access_token":"eyJhbGciOiJSUzI1NiIsImtpZCI6IlU3c0dGRldUTzlBekNhSzBqZURRM2dQZXBURVdWN2VhIn0.eyJzY29wZSI6WyJkYXRhOnJlYWQiXSwiY2xpZW50X2lkIjoibHRkT2JUbUFpQ0lRTndDdUxlUUdGWHpWN2xKTGRJeTkiLCJhdWQiOiJodHRwczovL2F1dG9kZXNrLmNvbS9hdWQvYWp3dGV4cDYwIiwianRpIjoiaTZLMWUwUndzM1pCbzRqdVNybWEyem9nRzRIT0gyR2dVQWtUU0Y2dm9Wa2syY1FyaWFBQ3ZpNGZIQ2tReGtDRyIsImV4cCI6MTYzODAyOTkzMX0.XAadU6vBNfDDwNlxhC5c2k1yW38iDNEFNEJNVo4oIL4NeO_UrnJs03cHcb2d5jddwY01AHu3_68ttEp_Kju3NEilxrWreg_A2D7p_opbKXrM-X8QsmVEtmcuONKAjXQk08vkQUU_385KGyNWZcjNcck4w44G8TbyXud-mXi3X1o6TXokdDtSvKwBChwPrDpy3uQMpytqpVcumM3xxhnzR1cRDw1VtaaXUqep3I16fVx9AK68PJfwt8cRt54iLOGFKSIqGNOSNtZ7eBUzwi4cks5ghHWBi0jGq5_v-HV-sJDJiM_xR7RdBPdJEqeFYGiMpWmxmtjUbXMNMTPl7LW_9A","token_type":"Bearer","expires_in":3599}'
+    return render_template('index.html', h2=f'Привет', body=f'{text}')
 
 
 def autodesk_auth_l2():
@@ -25,7 +27,7 @@ def autodesk_auth_l2():
         'scope': 'data:read'
     }
     req = requests.post(url, data=params, headers=headers)
-    print(req.text)
+    return req.text
 
 
 def main():
@@ -34,4 +36,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
+
