5409f7e17d89967b8a8838f7b5beaa1ba168c78d
Viktor Kolesnikov <kalach72ru@yandex.ru>
2021-11-28 19:04:04 +0500

    шаблон, скрипт, табы

--- static/script.js
+++ static/script.js
@@ -1,5 +1,8 @@
 var btn_get_token = document.getElementById('btn_get_token')
 
 btn_get_token.onclick = function (){
-        alert('Button!!!')
+        var client_id = document.getElementById('client_id').value
+        // var client_secret = document.getElementById('client_secret').value
+        var client_secret = $('#client_secret').val()
+        alert(client_id+':'+client_secret)
 }
